716d67e978f6022432e0179c7b8fbd5582ad282a
alvarogonmar <alvarogonmar0@gmail.com>
2025-10-01 12:50:52 -0600

    Add input prompt for user name in savings calculator

--- Calculadora-Ahorros.py
+++ Calculadora-Ahorros.py
@@ -1,3 +1,5 @@
 # con la función print() desplegamos texto en pantalla 
 print("***Bienvendo a la calculadora de ahorro***")
 
+# usamos la función input() para ingresar dato con el teclado y asignarlo a la variable nombre
+print("Ingresa tu nombre:")

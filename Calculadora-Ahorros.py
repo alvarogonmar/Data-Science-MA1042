a68f85a402d72cde475afb33648b8aeb0c8c5f65
alvarogonmar <alvarogonmar0@gmail.com>
2025-10-01 12:51:14 -0600

    Fix indentation and comments in savings calculator

--- Calculadora-Ahorros.py
+++ Calculadora-Ahorros.py
@@ -4,3 +4,6 @@ print("***Bienvendo a la calculadora de ahorro***")
 # usamos la función input() para ingresar dato con el teclado y asignarlo a la variable nombre
 print("Ingresa tu nombre:")
 nombre = input()
+
+# usamos la función float() para convertir texto a número decimal y int() a número entero
+saldo_inicial = float(input("Ingresa tu saldo inicial: "))

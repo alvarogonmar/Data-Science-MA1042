3647ca636cf0793f76f717b0d221d25c24fb767d
alvarogonmar <alvarogonmar0@gmail.com>
2025-10-03 16:49:03 -0600

    Add input prompt for user name in interest calculator

--- Calculadora-Interes.py
+++ Calculadora-Interes.py
@@ -3,3 +3,4 @@ print("***Bienvendo a la calculadora de intereses***")
 # asignamos valores a las variables necesarias
 tasa_interes = 0.08
 interes_acumulado = 0
+nombre = input("Ingresa tu nombre: ")

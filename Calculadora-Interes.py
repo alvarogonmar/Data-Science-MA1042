9846cc7581a2966103430ac1517730352dc6e944
alvarogonmar <alvarogonmar0@gmail.com>
2025-10-03 16:49:26 -0600

    Add input prompt for deposit amount in interest calculator

--- Calculadora-Interes.py
+++ Calculadora-Interes.py
@@ -5,3 +5,4 @@ tasa_interes = 0.08
 interes_acumulado = 0
 nombre = input("Ingresa tu nombre: ")
 saldo = int(input("Ingresa tu saldo inicial: "))
+deposito = int(input("Ingresa la cantidad a depositar en octubre: "))
